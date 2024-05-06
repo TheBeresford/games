5aeb6ee16926bfc97475990057be7889a4dca8cf
Jack Beresford <jack.beresford46@gmail.com>
2024-05-06 15:36:31 +0300

    updating logic

--- bin/game_logic.py
+++ bin/game_logic.py
@@ -4,10 +4,9 @@ import pandas as pd
 
 class game_board:
     def __init__(self, ) -> None:
+        self.ingredients = pd.read_csv('../data/ingredients.csv', delimiter=';', header=0)
+        self.board_positions = pd.read_csv('../data/board_positions.csv', delimiter=';', header=0)
 
 if __name__ == "__main__":
-
-    ingredients = pd.read_csv('../data/ingredients.csv', delimiter = ";", header=0)
-    board_positions = pd.read_csv('../data/board_positions.csv', delimiter=';', header=0)
 
     print(ingredients)
